2c2dee58ec77ca8a280b0114874f8d9ee630704d
xuzhanfu <7357288@qq.com>
2020-03-15 20:31:40 +0800

    configure global nacos

--- mate-core/mate-starter-common/src/main/java/vip/mate/common/constant/MateConstant.java
+++ mate-core/mate-starter-common/src/main/java/vip/mate/common/constant/MateConstant.java
@@ -1,39 +1,36 @@
-/*
- * Copyright 2019-2028 Beijing Daotiandi Technology Co., Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * Author: xuzhanfu (7333791@qq.com)
- */
 package vip.mate.common.constant;
 
-/**
- * 系统常量
- *
- * @author xuzhanfu
- * @date 2019-10-09 10:15
- **/
-public interface MateConstant {
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class MateConstant {
+
+    /**
+     * Spring 应用名 prop key
+     */
+    public static final String SPRING_APP_NAME_KEY = "spring.application.name";
+
+    /**
+     * request id mdc key
+     */
+    public static final String REQUEST_ID_MDC_KEY = "mdcReqId";
+
+    /**
+     * request id key
+     */
+    public static final String REQUEST_ID_KEY = "mate_request_id";
 
     /**
      * 默认为空消息
      */
-    String DEFAULT_NULL_MESSAGE = "承载数据为空";
+    public static final String DEFAULT_NULL_MESSAGE = "承载数据为空";
     /**
      * 默认成功消息
      */
-    String DEFAULT_SUCCESS_MESSAGE = "操作成功";
+    public static final String DEFAULT_SUCCESS_MESSAGE = "操作成功";
     /**
      * 默认失败消息
      */
-    String DEFAULT_FAIL_MESSAGE = "操作失败";
+    public static final String DEFAULT_FAIL_MESSAGE = "操作失败";
+
 }
