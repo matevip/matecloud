8a18daa7438cbc98ff4e85959972f80945f5d6fe
xuzhanfu <7357288@qq.com>
2020-03-22 22:49:52 +0800

    add mate-admin module

---
+++ mate-admin/mate-admin-app/src/main/java/vip/mate/admin/MateAdminApp.java
@@ -0,0 +1,15 @@
+package vip.mate.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * 管理系统后台启动类
+ * @author xzf
+ */
+@SpringBootApplication
+public class MateAdminApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MateAdminApp.class, args);
+    }
+}
