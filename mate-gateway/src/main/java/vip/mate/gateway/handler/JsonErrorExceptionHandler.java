d1c612aebc07383bf06e7a6142f42fc8d512ac56
xuzhanfu <7357288@qq.com>
2020-04-20 23:01:19 +0800

    网关自定义异常处理

---
+++ mate-gateway/src/main/java/vip/mate/gateway/handler/JsonErrorExceptionHandler.java
@@ -0,0 +1,63 @@
+package vip.mate.gateway.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.boot.autoconfigure.web.ErrorProperties;
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
+import org.springframework.boot.autoconfigure.web.reactive.error.DefaultErrorWebExceptionHandler;
+import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.reactive.function.server.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 自定义的JsonErrorWebExceptionHandler异常处理类
+ * @author xuzf
+ * date: 2020-4-20
+ * 参考文档：https://www.cnblogs.com/throwable/p/10848879.html
+ */
+@Slf4j
+public class JsonErrorExceptionHandler extends DefaultErrorWebExceptionHandler {
+
+    public JsonErrorExceptionHandler(ErrorAttributes errorAttributes,
+                                     ResourceProperties resourceProperties,
+                                     ErrorProperties errorProperties,
+                                     ApplicationContext applicationContext) {
+        super(errorAttributes, resourceProperties, errorProperties, applicationContext);
+    }
+
+    @Override
+    protected Map<String, Object> getErrorAttributes(ServerRequest request, boolean includeStackTrace) {
+        // 这里其实可以根据异常类型进行定制化逻辑
+        Throwable error = super.getError(request);
+        int code = HttpStatus.INTERNAL_SERVER_ERROR.value();
+        if (error instanceof FileNotFoundException) {
+            code = HttpStatus.NOT_FOUND.value();
+        } else if (error instanceof ResponseStatusException && StringUtils.contains(error.getMessage(), HttpStatus.NOT_FOUND.toString())) {
+            code = HttpStatus.NOT_FOUND.value();
+        }
+        Map<String, Object> errorAttributes = new HashMap<>(8);
+        errorAttributes.put("message", error.getMessage());
+        errorAttributes.put("code", code);
+        errorAttributes.put("method", request.methodName());
+        errorAttributes.put("path", request.path());
+        return errorAttributes;
+    }
+
+    @Override
+    @SuppressWarnings("all")
+    protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
+        return RouterFunctions.route(RequestPredicates.all(), this::renderErrorResponse);
+    }
+
+    @Override
+    protected int getHttpStatus(Map<String, Object> errorAttributes) {
+        return HttpStatus.INTERNAL_SERVER_ERROR.value();
+    }
+
+}
