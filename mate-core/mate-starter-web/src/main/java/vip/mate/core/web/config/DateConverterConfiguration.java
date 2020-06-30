7a0e757a0ca9b3c1f1eb23a588451699dbba4981
xuzhanfu <7357288@qq.com>
2020-07-01 00:19:25 +0800

    调整网关request日志打印格式

---
+++ mate-core/mate-starter-web/src/main/java/vip/mate/core/web/config/DateConverterConfiguration.java
@@ -0,0 +1,114 @@
+package vip.mate.core.web.config;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.convert.converter.Converter;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+import static java.time.format.DateTimeFormatter.ofPattern;
+
+/**
+ * 日期格式全局配置
+ * @author pangu
+ * @link https://juejin.im/post/5e940626e51d4546f5790979
+ * @link https://blog.csdn.net/weixin_44600430/article/details/105512891
+ */
+@Configuration
+@ConditionalOnClass(ObjectMapper.class)
+@AutoConfigureBefore(JacksonAutoConfiguration.class)
+public class DateConverterConfiguration {
+
+    private static final String YYYY_MM_DD = "yyyy-MM-dd";
+    private static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
+
+    @Bean
+    @Primary
+    public ObjectMapper serializingObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer());
+        javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer());
+        objectMapper.registerModule(javaTimeModule);
+        return objectMapper;
+    }
+
+    public class LocalDateTimeSerializer extends JsonSerializer<LocalDateTime> {
+        @Override
+        public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+            gen.writeString(value.format(ofPattern(YYYY_MM_DD_HH_MM_SS)));
+        }
+    }
+
+    public class LocalDateTimeDeserializer extends JsonDeserializer<LocalDateTime> {
+        @Override
+        public LocalDateTime deserialize(JsonParser p, DeserializationContext deserializationContext) throws IOException {
+            return LocalDateTime.parse(p.getValueAsString(), ofPattern(YYYY_MM_DD_HH_MM_SS));
+        }
+    }
+
+
+    /**
+     * 自定义类型转换,HTTP请求日期字符串转换日期类型,
+     * 相当于以前设置进 ConversionServiceFactoryBean
+     *
+     * @return Converter<java.lang.String,java.util.Date>
+     */
+    @Bean
+    public Converter<String, LocalDateTime> localDateTimeConverter() {
+        return new Converter<String, LocalDateTime>() {
+            @Override
+            public LocalDateTime convert(String source) {
+                try {
+                    return parse(source);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                throw new RuntimeException("日期解析错误");
+            }
+        };
+    }
+
+    /**
+     * 根据字符串进行解析,将Date转LocalDateTime
+     *
+     * @param source 日期字符串
+     * @return java.time.LocalDateTime
+     */
+    public LocalDateTime parse(String source) throws ParseException {
+        if (StringUtils.isBlank(source)) {
+            return null;
+        }
+        DateFormat format;
+        source = source.trim();
+        //判断是否yyyy-MM-dd格式
+        if(source.matches("^\\d{4}-\\d{1,2}-\\d{1,2}$")){
+            format = new SimpleDateFormat(YYYY_MM_DD);
+            Date date = format.parse(source);
+            return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+            //判断是否yyyy-MM-dd HH:mm:ss格式
+        }else if(source.matches("^\\d{4}-\\d{1,2}-\\d{1,2} {1}\\d{1,2}:\\d{1,2}:\\d{1,2}$")){
+            format = new SimpleDateFormat(YYYY_MM_DD_HH_MM_SS);
+            Date date = format.parse(source);
+            return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+        }else {
+            throw new IllegalArgumentException("Invalid false value " + source);
+        }
+    }
+
+}
---
+++ mate-core/mate-starter-web/src/main/java/vip/mate/core/web/datatype/MateJavaTimeModule.java
@@ -0,0 +1,41 @@
+package vip.mate.core.web.datatype;
+
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.PackageVersion;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class MateJavaTimeModule extends SimpleModule {
+    public static final String PATTERN_DATETIME = "yyyy-MM-dd HH:mm:ss";
+    public static final String PATTERN_DATETIME_MINI = "yyyyMMddHHmmss";
+    public static final String PATTERN_DATE = "yyyy-MM-dd";
+    public static final String PATTERN_TIME = "HH:mm:ss";
+
+
+    /**
+     * java 8 时间格式化
+     */
+    public static final DateTimeFormatter DATETIME_FORMATTER = DateTimeFormatter.ofPattern(PATTERN_DATETIME);
+    public static final DateTimeFormatter DATETIME_MINI_FORMATTER = DateTimeFormatter.ofPattern(PATTERN_DATETIME_MINI);
+    public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern(PATTERN_DATE);
+    public static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern(PATTERN_TIME);
+
+    public MateJavaTimeModule(){
+        super(PackageVersion.VERSION);
+        this.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(DATETIME_FORMATTER));
+        this.addDeserializer(LocalDate.class, new LocalDateDeserializer(DATE_FORMATTER));
+        this.addDeserializer(LocalTime.class, new LocalTimeDeserializer(TIME_FORMATTER));
+        this.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DATETIME_FORMATTER));
+        this.addSerializer(LocalDate.class, new LocalDateSerializer(DATE_FORMATTER));
+        this.addSerializer(LocalTime.class, new LocalTimeSerializer(TIME_FORMATTER));
+    }
+}
