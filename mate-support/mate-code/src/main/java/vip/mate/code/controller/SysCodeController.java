3bd5b7cbbece5af4a16c853ba569556534c45ec1
pangu <7333791@qq.com>
2020-07-11 07:06:46 +0800

    升级spring cloud版本至Hoxton.SR6,nacos-client版本至1.3.1

--- mate-support/mate-code/src/main/java/vip/mate/code/controller/SysCodeController.java
+++ mate-support/mate-code/src/main/java/vip/mate/code/controller/SysCodeController.java
@@ -1,16 +1,29 @@
+/*
+ * Copyright 2020-2030, MateCloud, DAOTIANDI Technology Inc All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * Author: pangu(7333791@qq.com)
+ */
 package vip.mate.code.controller;
 
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.generator.config.*;
 import com.baomidou.mybatisplus.generator.config.builder.ConfigBuilder;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
-import com.google.common.collect.Maps;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -27,7 +40,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 @Slf4j
