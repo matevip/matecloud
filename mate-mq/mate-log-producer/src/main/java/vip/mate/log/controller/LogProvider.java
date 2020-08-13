69b06957eea3632a69f36e40f66917632f658e32
xuzhanfu <7357288@qq.com>
2020-08-13 19:29:11 +0800

    增加消费者demo和打印消费日志

--- mate-mq/mate-log-producer/src/main/java/vip/mate/log/controller/LogProvider.java
+++ mate-mq/mate-log-producer/src/main/java/vip/mate/log/controller/LogProvider.java
@@ -1,12 +1,16 @@
 package vip.mate.log.controller;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vip.mate.core.kafka.channel.LogChannel;
 import vip.mate.log.entity.LogMessage;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 public class LogProvider {
@@ -16,6 +20,17 @@ public class LogProvider {
     @GetMapping("/sendMessage")
     public boolean sendMessage(LogMessage logMessage) {
         return logChannel.sendLogMessage().send(MessageBuilder.withPayload(logMessage).build());
+    }
+
+
+    @GetMapping("/sendMessageStr")
+    public boolean sendMessage(String message) {
+        return logChannel.sendLogMessage().send(MessageBuilder.withPayload(message).build());
+    }
+
+    @StreamListener(LogChannel.LOG_INPUT)
+    public void receive(Message<String> message) {
+        log.error(message.getPayload());
     }
 
 }
