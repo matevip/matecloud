d0b2b45c071420050898a4e385a2efb7d5dfd36d
matevip <7333791@qq.com>
2020-08-17 23:23:10 +0800

    mate-system增加druid监控功能

---
+++ mate-core/mate-starter-database/src/main/java/vip/mate/core/database/config/DruidConfiguration.java
@@ -0,0 +1,58 @@
+package vip.mate.core.database.config;
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import vip.mate.core.common.factory.YamlPropertySourceFactory;
+
+@Configuration
+@PropertySource(factory = YamlPropertySourceFactory.class, value = "classpath:mate-druid.yml")
+public class DruidConfiguration {
+    /**
+     * 注册一个StatViewServlet
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean DruidStatViewServle(){
+
+        //org.springframework.boot.context.embedded.ServletRegistrationBean提供类的进行注册.
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+
+        //添加初始化参数：initParams
+
+        //白名单：
+        servletRegistrationBean.addInitParameter("allow","127.0.0.1");
+
+        //IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not permitted to view this page.
+        servletRegistrationBean.addInitParameter("deny","192.168.0.114");
+
+        //登录查看信息的账号密码.
+        servletRegistrationBean.addInitParameter("loginUsername","matecloud");
+        servletRegistrationBean.addInitParameter("loginPassword","matecloud");
+
+        //是否能够重置数据.
+        servletRegistrationBean.addInitParameter("resetEnable","false");
+        return servletRegistrationBean;
+    }
+
+    /**
+     * 注册一个：filterRegistrationBean
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean druidStatFilter(){
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+
+        //添加过滤规则.
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        //添加不需要忽略的格式信息.
+        filterRegistrationBean.addInitParameter("exclusions","*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+
+        return filterRegistrationBean;
+    }
+}
--- mate-platform/mate-system/src/main/java/vip/mate/system/MateSystemServer.java
+++ mate-platform/mate-system/src/main/java/vip/mate/system/MateSystemServer.java
@@ -2,6 +2,7 @@ package vip.mate.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import vip.mate.core.feign.annotation.EnableMateFeign;
 
 /*
