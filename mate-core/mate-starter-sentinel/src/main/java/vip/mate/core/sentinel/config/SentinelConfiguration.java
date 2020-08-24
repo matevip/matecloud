bafa77239957cc9ad5b639b271141ad7534bc8fb
matevip <7333791@qq.com>
2020-08-25 00:11:46 +0800

    独立封装sentinel模块，并升级版本至1.8.0

---
+++ mate-core/mate-starter-sentinel/src/main/java/vip/mate/core/sentinel/config/SentinelConfiguration.java
@@ -0,0 +1,51 @@
+package vip.mate.core.sentinel.config;
+
+import com.alibaba.csp.sentinel.adapter.spring.webflux.callback.BlockRequestHandler;
+import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.BlockExceptionHandler;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import vip.mate.core.common.api.Result;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Configuration
+public class SentinelConfiguration {
+
+    /**
+     * 限流、熔断统一处理类
+     */
+    @Configuration
+    @ConditionalOnClass(HttpServletRequest.class)
+    public static class WebmvcHandler {
+        @Bean
+        public BlockExceptionHandler webmvcBlockExceptionHandler() {
+            return (request, response, e) -> {
+                response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+                Result<?> result = Result.fail(e.getMessage());
+                response.getWriter().print(JSONObject.toJSONString(result));
+            };
+        }
+
+    }
+
+    /**
+     * 限流、熔断统一处理类
+     */
+    @Configuration
+    @ConditionalOnClass(ServerResponse.class)
+    public static class WebfluxHandler {
+        @Bean
+        public BlockRequestHandler webfluxBlockExceptionHandler() {
+            return (exchange, t) ->
+                    ServerResponse.status(HttpStatus.TOO_MANY_REQUESTS)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .body(BodyInserters.fromValue(Result.fail(t.getMessage())));
+        }
+    }
+}
