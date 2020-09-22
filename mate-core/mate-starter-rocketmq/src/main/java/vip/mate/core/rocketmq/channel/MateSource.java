c896b7f34c97419e69a8460274914dc0fa08ae50
matevip <7333791@qq.com>
2020-09-22 09:00:38 +0800

    生产者和消费者初步实例整合完成

--- mate-core/mate-starter-rocketmq/src/main/java/vip/mate/core/rocketmq/channel/MateSource.java
+++ mate-core/mate-starter-rocketmq/src/main/java/vip/mate/core/rocketmq/channel/MateSource.java
@@ -2,7 +2,6 @@ package vip.mate.core.rocketmq.channel;
 
 import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.messaging.MessageChannel;
-import vip.mate.core.common.util.StringPool;
 import vip.mate.core.rocketmq.constant.MessageConstant;
 
 /**
@@ -14,6 +13,7 @@ public interface MateSource {
 
 	/**
 	 * 短消息通道
+	 *
 	 * @return MessageChannel
 	 */
 	@Output(MessageConstant.SMS_MESSAGE_OUTPUT)
@@ -21,6 +21,7 @@ public interface MateSource {
 
 	/**
 	 * 邮件通道
+	 *
 	 * @return MessageChannel
 	 */
 	@Output(MessageConstant.EMAIL_MESSAGE_OUTPUT)
