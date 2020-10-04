beaa8656697da80d4bc639afb08461bf6be17b11
matevip <7333791@qq.com>
2020-10-04 08:10:43 +0800

    定时任务增加注释

--- mate-support/mate-job/src/main/java/vip/mate/job/MateJobServer.java
+++ mate-support/mate-job/src/main/java/vip/mate/job/MateJobServer.java
@@ -5,12 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * xxl-job启动器
- * @link https://gitee.com/xuxueli0323/xxl-job
+ *
  * @author pangu
+ * @link https://gitee.com/xuxueli0323/xxl-job
  */
 @SpringBootApplication
 public class MateJobServer {
-    public static void main(String[] args) {
-        SpringApplication.run(MateJobServer.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(MateJobServer.class, args);
+	}
 }
