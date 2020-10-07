09b1c0fbd3bc16573aa0840a1ebf9a5534cfd15e
matevip <7333791@qq.com>
2020-10-07 11:02:43 +0800

    优化启动类的一些警告，如validator和undertow

---
+++ mate-core/mate-starter-web/src/main/java/vip/mate/core/web/server/UndertowServerFactoryCustomizer.java
@@ -0,0 +1,23 @@
+package vip.mate.core.web.server;
+
+import io.undertow.server.DefaultByteBufferPool;
+import io.undertow.websockets.jsr.WebSocketDeploymentInfo;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+
+/**
+ * 解决undertow警告Buffer pool was not set on WebSocketDeploymentInfo
+ *
+ * @author pangu
+ */
+public class UndertowServerFactoryCustomizer implements WebServerFactoryCustomizer<UndertowServletWebServerFactory> {
+
+	@Override
+	public void customize(UndertowServletWebServerFactory factory) {
+		factory.addDeploymentInfoCustomizers(deploymentInfo -> {
+			WebSocketDeploymentInfo webSocketDeploymentInfo = new WebSocketDeploymentInfo();
+			webSocketDeploymentInfo.setBuffers(new DefaultByteBufferPool(false, 1024));
+			deploymentInfo.addServletContextAttribute("io.undertow.websockets.jsr.WebSocketDeploymentInfo", webSocketDeploymentInfo);
+		});
+	}
+}
