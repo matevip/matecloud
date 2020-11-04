8fbc4552e593b5e999eecf45db434c9d6d5216b0
matevip <7333791@qq.com>
2020-11-05 07:14:14 +0800

    升级至nacos 1.4.0

---
+++ mate-gateway/src/test/java/vip/mate/gateway/GatewayServerTests.java
@@ -0,0 +1,20 @@
+package vip.mate.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 网关服务测试类
+ *
+ * @author pangu
+ */
+@Slf4j
+@SpringBootTest
+public class GatewayServerTests {
+
+	@Test
+	void contextLoads() {
+		log.error("contextLoads");
+	}
+}
