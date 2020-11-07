95b32a47812871a515cca19edbab9fe523e0c05b
matevip <7333791@qq.com>
2020-11-07 09:34:07 +0800

    oss开源项目引用地址

--- mate-core/mate-starter-oss/src/main/java/vip/mate/core/oss/props/OssProperties.java
+++ mate-core/mate-starter-oss/src/main/java/vip/mate/core/oss/props/OssProperties.java
@@ -1,3 +1,19 @@
+/*
+ *    Copyright (c) 2018-2025, lengleng All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the pig4cloud.com developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: lengleng (wangiegie@gmail.com)
+ */
 package vip.mate.core.oss.props;
 
 import lombok.Data;
@@ -6,7 +22,8 @@ import java.io.Serializable;
 
 /**
  * AWS配置信息
- * @author pangu
+ * @author lengleng
+ * @author 858695266
  * <p>
  *     oss: enable: true
  *     endpoint: http://127.0.0.1:9000 #
