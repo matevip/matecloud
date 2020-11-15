41c12714f11391f8d05b8ad48c51057a2b8ee780
matevip <7333791@qq.com>
2020-11-15 18:01:17 +0800

    增加作者信息

--- mate-core/mate-starter-feign/src/main/java/org/springframework/cloud/openfeign/MateFeignClientsRegistrar.java
+++ mate-core/mate-starter-feign/src/main/java/org/springframework/cloud/openfeign/MateFeignClientsRegistrar.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2019-2029, Dreamlu 卢春梦 (596392912@qq.com & www.dreamlu.net).
+ * <p>
+ * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE 3.0;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.gnu.org/licenses/lgpl.html
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.cloud.openfeign;
 
 import lombok.Getter;
@@ -21,6 +36,11 @@ import vip.mate.core.feign.config.MateFeignAutoConfiguration;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * feign 自动配置
+ *
+ * @author L.cm
+ */
 public class MateFeignClientsRegistrar implements ImportBeanDefinitionRegistrar, BeanClassLoaderAware, EnvironmentAware {
 
     @Getter
