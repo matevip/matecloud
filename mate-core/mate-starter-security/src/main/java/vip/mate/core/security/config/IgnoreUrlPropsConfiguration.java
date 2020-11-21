9ede613d4348d84739fbfb1a7b528383081b710a
matevip <7333791@qq.com>
2020-11-21 11:54:29 +0800

    修改忽略参数从IgnoreUrlPropsConfiguration读取

--- mate-core/mate-starter-security/src/main/java/vip/mate/core/security/config/IgnoreUrlPropsConfiguration.java
+++ mate-core/mate-starter-security/src/main/java/vip/mate/core/security/config/IgnoreUrlPropsConfiguration.java
@@ -5,7 +5,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -15,11 +17,37 @@ import java.util.List;
  */
 @Data
 @RefreshScope
-@ConfigurationProperties(prefix = "ignore")
+@ConfigurationProperties(prefix = "mate.security.ignore")
 @Component
 public class IgnoreUrlPropsConfiguration {
+
+	/**
+	 * 认证中心默认忽略验证地址
+	 */
+	private static final String[] SECURITY_ENDPOINTS = {
+			"/auth/**",
+			"/oauth/token",
+			"/login/*",
+			"/actuator/**",
+			"/v3/api-docs",
+			"/doc.html",
+			"/webjars/**",
+			"**/favicon.ico",
+			"/swagger-resources/**"
+	};
 
 	private List<String> urls = new ArrayList<>();
 
 	private List<String> client = new ArrayList<>();
+
+	private List<String> ignoreSecurity = new ArrayList<>();
+
+	/**
+	 * 首次加载合并ENDPOINTS
+	 */
+	@PostConstruct
+	public void initIgnoreSecurity() {
+		Collections.addAll(ignoreSecurity, SECURITY_ENDPOINTS);
+	}
+
 }
