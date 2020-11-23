9566f20d985e111a867194809cf9af896c52d329
matevip <7333791@qq.com>
2020-11-23 19:12:37 +0800

    升级至mysql 8.0.22, knife4j 3.0.2

--- mate-core/mate-starter-common/src/main/java/vip/mate/core/common/constant/ComponentConstant.java
+++ mate-core/mate-starter-common/src/main/java/vip/mate/core/common/constant/ComponentConstant.java
@@ -2,25 +2,30 @@ package vip.mate.core.common.constant;
 
 import lombok.experimental.UtilityClass;
 
+/**
+ * 组件参数
+ *
+ * @author pangu
+ */
 @UtilityClass
 public class ComponentConstant {
 
-    /**
-     * OSS的code,通过此code获取oss默认值
-     */
-    public final String OSS_DEFAULT = "oss";
+	/**
+	 * OSS的code,通过此code获取oss默认值
+	 */
+	public final String OSS_DEFAULT = "oss";
 
-    public final String CODE_DEFAULT = "default";
+	public final String CODE_DEFAULT = "default";
 
-    public final String OSS_ENDPOINT = "endpoint";
+	public final String OSS_ENDPOINT = "endpoint";
 
-    public final String OSS_CUSTOM_DOMAIN = "customDomain";
+	public final String OSS_CUSTOM_DOMAIN = "customDomain";
 
-    public final String OSS_PATH_STYLE_ACCESS = "pathStyleAccess";
+	public final String OSS_PATH_STYLE_ACCESS = "pathStyleAccess";
 
-    public final String OSS_ACCESS_KEY = "accessKey";
+	public final String OSS_ACCESS_KEY = "accessKey";
 
-    public final String OSS_SECRET_KEY = "secretKey";
+	public final String OSS_SECRET_KEY = "secretKey";
 
-    public final String OSS_BUCKET_NAME = "bucketName";
+	public final String OSS_BUCKET_NAME = "bucketName";
 }
