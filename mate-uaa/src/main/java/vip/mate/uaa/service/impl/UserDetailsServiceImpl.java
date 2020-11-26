9bf14a0b19ae0c2a8bb3f08cd715661ca8601393
matevip <7333791@qq.com>
2020-11-27 07:13:45 +0800

    优化uaa部分代码和注释

--- mate-uaa/src/main/java/vip/mate/uaa/service/impl/UserDetailsServiceImpl.java
+++ mate-uaa/src/main/java/vip/mate/uaa/service/impl/UserDetailsServiceImpl.java
@@ -20,68 +20,72 @@ import java.util.Collection;
 
 /**
  * 用户详情实现类
+ *
  * @author pangu
  */
 @Slf4j
 @Service
 public class UserDetailsServiceImpl implements MateUserDetailsService {
 
-    @Resource
-    private ISysUserProvider sysUserProvider;
+	public static final String ENABLE = "0";
+	public static final String DISABLE = "1";
 
-    @Override
-    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        UserInfo userInfo = sysUserProvider.getUserByUserName(userName).getData();
-        if (userInfo == null) {
-            throw new TokenException("该用户：" + userName + "不存在");
-        }
-        userInfo.setType(Oauth2Constant.LOGIN_USERNAME_TYPE);
-        userInfo.setUserName(userName);
-        return getUserDetails(userInfo);
+	@Resource
+	private ISysUserProvider sysUserProvider;
 
-    }
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		UserInfo userInfo = sysUserProvider.getUserByUserName(userName).getData();
+		if (userInfo == null) {
+			throw new TokenException("该用户：" + userName + "不存在");
+		}
+		userInfo.setType(Oauth2Constant.LOGIN_USERNAME_TYPE);
+		userInfo.setUserName(userName);
+		return getUserDetails(userInfo);
 
-    @Override
-    public UserDetails loadUserByMobile(String mobile) throws UsernameNotFoundException {
-        UserInfo userInfo = sysUserProvider.getUserByMobile(mobile).getData();
-        if (userInfo == null) {
-            throw new TokenException("该用户：" + mobile + "不存在");
-        }
-        userInfo.setType(Oauth2Constant.LOGIN_MOBILE_TYPE);
-        userInfo.setUserName(mobile);
-        return getUserDetails(userInfo);
-    }
+	}
 
-    @Override
-    public UserDetails loadUserBySocial(String openId) throws UsernameNotFoundException {
-        String userName = "admin";
-        UserInfo userInfo = sysUserProvider.getUserByUserName(userName).getData();
-        if (userInfo == null) {
-            throw new TokenException("该用户：" + userName + "不存在");
-        }
-        userInfo.setType(Oauth2Constant.LOGIN_USERNAME_TYPE);
-        userInfo.setUserName(userName);
-        return getUserDetails(userInfo);
-    }
+	@Override
+	public UserDetails loadUserByMobile(String mobile) throws UsernameNotFoundException {
+		UserInfo userInfo = sysUserProvider.getUserByMobile(mobile).getData();
+		if (userInfo == null) {
+			throw new TokenException("该用户：" + mobile + "不存在");
+		}
+		userInfo.setType(Oauth2Constant.LOGIN_MOBILE_TYPE);
+		userInfo.setUserName(mobile);
+		return getUserDetails(userInfo);
+	}
+
+	@Override
+	public UserDetails loadUserBySocial(String openId) throws UsernameNotFoundException {
+		String userName = "admin";
+		UserInfo userInfo = sysUserProvider.getUserByUserName(userName).getData();
+		if (userInfo == null) {
+			throw new TokenException("该用户：" + userName + "不存在");
+		}
+		userInfo.setType(Oauth2Constant.LOGIN_USERNAME_TYPE);
+		userInfo.setUserName(userName);
+		return getUserDetails(userInfo);
+	}
 
 
-    private UserDetails getUserDetails(UserInfo userInfo) {
-        if (ObjectUtils.isEmpty(userInfo)) {
-            log.info("该用户：{} 不存在！", userInfo.getUserName());
-            throw new TokenException("该用户：" + userInfo.getUserName() + "不存在");
-        } else if (userInfo.getSysUser().getStatus().equals("1")) {
-            log.info("该用户：{} 已被停用!", userInfo.getUserName());
-            throw new TokenException("对不起，您的账号：" + userInfo.getUserName() + " 已停用");
-        }
-        SysUser user = userInfo.getSysUser();
-        log.info("用户名：{}", userInfo.getSysUser().getAccount());
-        Collection<? extends GrantedAuthority> authorities
-                = AuthorityUtils.createAuthorityList("*:*:*");
-        log.info("authorities: {}", authorities);
-        return new MateUser(user.getId(), userInfo.getType(), user.getDepartId(), user.getRoleId(), user.getTelephone(), user.getAvatar(),
-                user.getTenantId(), userInfo.getUserName(), user.getPassword(), user.getStatus().equals("0") ? true : false,
-                true, true, true,
-                authorities);
-    }
+	private UserDetails getUserDetails(UserInfo userInfo) {
+		if (ObjectUtils.isEmpty(userInfo)) {
+			log.info("该用户：{} 不存在！", userInfo.getUserName());
+			throw new TokenException("该用户：" + userInfo.getUserName() + "不存在");
+		} else if (DISABLE.equals(userInfo.getSysUser().getStatus())) {
+			log.info("该用户：{} 已被停用!", userInfo.getUserName());
+			throw new TokenException("对不起，您的账号：" + userInfo.getUserName() + " 已停用");
+		}
+		SysUser user = userInfo.getSysUser();
+		log.info("用户名：{}", userInfo.getSysUser().getAccount());
+		Collection<? extends GrantedAuthority> authorities
+				= AuthorityUtils.createAuthorityList("*:*:*");
+		log.info("authorities: {}", authorities);
+		return new MateUser(user.getId(), userInfo.getType(), user.getDepartId(), user.getRoleId(), user.getTelephone(), user.getAvatar(),
+				user.getTenantId(), userInfo.getUserName(), user.getPassword(), ENABLE.equals(user.getStatus()),
+				true, true, true,
+				authorities);
+	}
 
 }
