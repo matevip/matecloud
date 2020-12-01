21a081b9bfb630b1faef8fad45ae63e1bc4290c3
matevip <7333791@qq.com>
2020-12-01 11:18:51 +0800

    JetCache修改为SpringData模式

--- mate-core/mate-starter-jetcache/src/main/java/vip/mate/core/jetcache/config/JetCacheConfiguration.java
+++ mate-core/mate-starter-jetcache/src/main/java/vip/mate/core/jetcache/config/JetCacheConfiguration.java
@@ -7,7 +7,7 @@ import com.alicp.jetcache.anno.support.GlobalCacheConfig;
 import com.alicp.jetcache.anno.support.SpringConfigProvider;
 import com.alicp.jetcache.embedded.EmbeddedCacheBuilder;
 import com.alicp.jetcache.embedded.LinkedHashMapCacheBuilder;
-import com.alicp.jetcache.redis.lettuce.RedisLettuceCacheBuilder;
+import com.alicp.jetcache.redis.springdata.RedisSpringDataCacheBuilder;
 import com.alicp.jetcache.support.FastjsonKeyConvertor;
 import com.alicp.jetcache.support.JavaValueDecoder;
 import com.alicp.jetcache.support.JavaValueEncoder;
@@ -21,49 +21,53 @@ import vip.mate.core.common.factory.YamlPropertySourceFactory;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * JetCache配置
+ *
+ * @author pangu
+ */
 @Configuration
 @EnableMethodCache(basePackages = "vip.mate.system")
 @EnableCreateCacheAnnotation
 @PropertySource(factory = YamlPropertySourceFactory.class, value = "classpath:mate-jetcache.yml")
 public class JetCacheConfiguration {
-    @Bean
-    public RedisClient redisClient(){
-        RedisClient client = RedisClient.create("redis://127.0.0.1");
-        client.setOptions(ClientOptions.builder().
-                disconnectedBehavior(ClientOptions.DisconnectedBehavior.REJECT_COMMANDS)
-                .build());
-        return client;
-    }
+	@Bean
+	public RedisClient redisClient() {
+		RedisClient client = RedisClient.create("redis://127.0.0.1");
+		client.setOptions(ClientOptions.builder().
+				disconnectedBehavior(ClientOptions.DisconnectedBehavior.REJECT_COMMANDS)
+				.build());
+		return client;
+	}
 
-    @Bean
-    public SpringConfigProvider springConfigProvider() {
-        return new SpringConfigProvider();
-    }
+	@Bean
+	public SpringConfigProvider springConfigProvider() {
+		return new SpringConfigProvider();
+	}
 
-    @Bean
-    public GlobalCacheConfig config(SpringConfigProvider configProvider, RedisClient redisClient){
-        Map localBuilders = new HashMap();
-        EmbeddedCacheBuilder localBuilder = LinkedHashMapCacheBuilder
-                .createLinkedHashMapCacheBuilder()
-                .keyConvertor(FastjsonKeyConvertor.INSTANCE);
-        localBuilders.put(CacheConsts.DEFAULT_AREA, localBuilder);
+	@Bean
+	public GlobalCacheConfig config(SpringConfigProvider configProvider, RedisClient redisClient) {
+		Map localBuilders = new HashMap();
+		EmbeddedCacheBuilder localBuilder = LinkedHashMapCacheBuilder
+				.createLinkedHashMapCacheBuilder()
+				.keyConvertor(FastjsonKeyConvertor.INSTANCE);
+		localBuilders.put(CacheConsts.DEFAULT_AREA, localBuilder);
 
-        Map remoteBuilders = new HashMap();
-        RedisLettuceCacheBuilder remoteCacheBuilder = RedisLettuceCacheBuilder.createRedisLettuceCacheBuilder()
-                .keyConvertor(FastjsonKeyConvertor.INSTANCE)
-                .valueEncoder(JavaValueEncoder.INSTANCE)
-                .valueDecoder(JavaValueDecoder.INSTANCE)
-                .redisClient(redisClient);
-        remoteBuilders.put(CacheConsts.DEFAULT_AREA, remoteCacheBuilder);
+		Map remoteBuilders = new HashMap(6);
+		RedisSpringDataCacheBuilder<?> redisSpringDataCacheBuilder = RedisSpringDataCacheBuilder.createBuilder()
+				.keyConvertor(FastjsonKeyConvertor.INSTANCE)
+				.valueEncoder(JavaValueEncoder.INSTANCE)
+				.valueDecoder(JavaValueDecoder.INSTANCE);
+		remoteBuilders.put(CacheConsts.DEFAULT_AREA, redisSpringDataCacheBuilder);
 
-        GlobalCacheConfig globalCacheConfig = new GlobalCacheConfig();
-        // globalCacheConfig.setConfigProvider(configProvider);//for jetcache <=2.5
-        globalCacheConfig.setLocalCacheBuilders(localBuilders);
-        globalCacheConfig.setRemoteCacheBuilders(remoteBuilders);
-        globalCacheConfig.setStatIntervalMinutes(15);
-        globalCacheConfig.setAreaInCacheName(false);
+		GlobalCacheConfig globalCacheConfig = new GlobalCacheConfig();
+		// globalCacheConfig.setConfigProvider(configProvider);//for jetcache <=2.5
+		globalCacheConfig.setLocalCacheBuilders(localBuilders);
+		globalCacheConfig.setRemoteCacheBuilders(remoteBuilders);
+		globalCacheConfig.setStatIntervalMinutes(15);
+		globalCacheConfig.setAreaInCacheName(false);
 
-        return globalCacheConfig;
-    }
+		return globalCacheConfig;
+	}
 
 }
