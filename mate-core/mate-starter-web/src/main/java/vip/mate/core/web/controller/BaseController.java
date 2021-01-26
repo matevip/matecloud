0b7ee1b464f9c21d5d21d47cce2883a662038ee5
matevip <7333791@qq.com>
2021-01-26 08:41:22 +0800

    优化工具类，并增加相应注释

--- mate-core/mate-starter-web/src/main/java/vip/mate/core/web/controller/BaseController.java
+++ mate-core/mate-starter-web/src/main/java/vip/mate/core/web/controller/BaseController.java
@@ -1,9 +1,7 @@
 package vip.mate.core.web.controller;
 
-import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
-import vip.mate.core.common.constant.MateConstant;
 import vip.mate.core.common.util.DateUtil;
 
 import java.beans.PropertyEditorSupport;
@@ -11,25 +9,23 @@ import java.util.Date;
 
 /**
  * 基础控制类
+ *
  * @author aristotle
  * @since 2020-06-01
  */
 public class BaseController {
 
-    /**
-     * 将前台传递过来的日期格式的字符串，自动转化为Date类型
-     */
-    @InitBinder
-    public void initBinder(WebDataBinder binder)
-    {
-        // Date 类型转换
-        binder.registerCustomEditor(Date.class, new PropertyEditorSupport()
-        {
-            @Override
-            public void setAsText(String text)
-            {
-                setValue(DateUtil.parseLocalDateTime(text, DateUtil.DATETIME_FORMATTER));
-            }
-        });
-    }
+	/**
+	 * 将前台传递过来的日期格式的字符串，自动转化为Date类型
+	 */
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		// Date 类型转换
+		binder.registerCustomEditor(Date.class, new PropertyEditorSupport() {
+			@Override
+			public void setAsText(String text) {
+				setValue(DateUtil.parseLocalDateTime(text, DateUtil.DATETIME_FORMATTER));
+			}
+		});
+	}
 }
--- mate-core/mate-starter-web/src/main/java/vip/mate/core/web/util/OssUtil.java
+++ mate-core/mate-starter-web/src/main/java/vip/mate/core/web/util/OssUtil.java
@@ -2,26 +2,31 @@ package vip.mate.core.web.util;
 
 import org.apache.commons.io.FilenameUtils;
 
+/**
+ * OSS处理工具类
+ *
+ * @author pangu
+ */
 public class OssUtil {
 
-    public static final String IMAGES_STR = "png,jpg,jpeg,gif,tif,bmp";
-    public static final String VIDEO_STR = "avi,wmv,mpeg,mp4,mov,flv,rm,rmvb,3gp";
+	public static final String IMAGES_STR = "png,jpg,jpeg,gif,tif,bmp";
+	public static final String VIDEO_STR = "avi,wmv,mpeg,mp4,mov,flv,rm,rmvb,3gp";
 
-    /**
-     * 根据文件名获取文件类型：1.图片　2.视频　3.其他
-     *
-     * @param fileName 文件名
-     * @return int
-     */
-    public static int getFileType(String fileName) {
-        String fileType = FilenameUtils.getExtension(fileName);
-        assert fileType != null;
-        int type = 3;
-        if (IMAGES_STR.contains(fileType)) {
-            type = 1;
-        } else if (VIDEO_STR.contains(fileType)) {
-            type = 2;
-        }
-        return type;
-    }
+	/**
+	 * 根据文件名获取文件类型：1.图片　2.视频　3.其他
+	 *
+	 * @param fileName 文件名
+	 * @return int
+	 */
+	public static int getFileType(String fileName) {
+		String fileType = FilenameUtils.getExtension(fileName);
+		assert fileType != null;
+		int type = 3;
+		if (IMAGES_STR.contains(fileType)) {
+			type = 1;
+		} else if (VIDEO_STR.contains(fileType)) {
+			type = 2;
+		}
+		return type;
+	}
 }
