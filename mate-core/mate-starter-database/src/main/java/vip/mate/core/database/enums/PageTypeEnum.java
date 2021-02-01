7db751d6915ae87835835d4672b7180eafe852d3
matevip <7333791@qq.com>
2021-02-01 20:03:37 +0800

    优化代码生成的分页工具类

---
+++ mate-core/mate-starter-database/src/main/java/vip/mate/core/database/enums/PageTypeEnum.java
@@ -0,0 +1,25 @@
+package vip.mate.core.database.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 分类类型
+ *
+ * @author pangu
+ */
+@Getter
+@AllArgsConstructor
+public enum PageTypeEnum {
+	/**
+	 * 默认当前页
+	 */
+	CURRENT(1),
+
+	/**
+	 * 默认每页数据
+	 */
+	SIZE(20);
+
+	private final Integer number;
+}
