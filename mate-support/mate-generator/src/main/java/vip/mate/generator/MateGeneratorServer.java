c456b2f0f3852d57e70d94d7785fb86e3855c866
matevip <7333791@qq.com>
2021-02-23 19:43:51 +0800

    feature: 代码生成增强启动类

---
+++ mate-support/mate-generator/src/main/java/vip/mate/generator/MateGeneratorServer.java
@@ -0,0 +1,17 @@
+package vip.mate.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 代码生成增强版本启动类
+ *
+ * @author pangu
+ * @since 2.3.8
+ */
+@SpringBootApplication
+public class MateGeneratorServer {
+	public static void main(String[] args) {
+		SpringApplication.run(MateGeneratorServer.class, args);
+	}
+}
