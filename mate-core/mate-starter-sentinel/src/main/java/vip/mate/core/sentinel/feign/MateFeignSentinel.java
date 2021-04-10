ab692bc3bf967558f6eb7ea96087e240136e7872
matevip <7333791@qq.com>
2021-04-10 14:33:13 +0800

    fix: 解决Hoxton SR10版本出现feign与sentinel依赖冲突的问题

---
+++ mate-core/mate-starter-sentinel/src/main/java/vip/mate/core/sentinel/feign/MateFeignSentinel.java
@@ -0,0 +1,116 @@
+package vip.mate.core.sentinel.feign;
+
+import com.alibaba.cloud.sentinel.feign.SentinelContractHolder;
+import com.alibaba.cloud.sentinel.feign.SentinelFeign;
+import feign.Contract;
+import feign.Feign;
+import feign.InvocationHandlerFactory;
+import feign.Target;
+import org.springframework.beans.BeansException;
+import org.springframework.cloud.openfeign.FallbackFactory;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.FeignContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.core.annotation.AnnotationUtils;
+import org.springframework.util.StringUtils;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+/**
+ * 重写{@link SentinelFeign} 解决Feign与Sentinel冲突的问题
+ * 这个问题在Hoxton SR10版本出现
+ *
+ * @author pangu
+ */
+public final class MateFeignSentinel {
+
+	private MateFeignSentinel() {
+
+	}
+
+	public static MateFeignSentinel.Builder builder() {
+		return new MateFeignSentinel.Builder();
+	}
+
+	public static final class Builder extends Feign.Builder implements ApplicationContextAware {
+
+		private Contract contract = new Contract.Default();
+
+		private ApplicationContext applicationContext;
+
+		private FeignContext feignContext;
+
+		@Override
+		public Feign.Builder invocationHandlerFactory(InvocationHandlerFactory invocationHandlerFactory) {
+			throw new UnsupportedOperationException();
+		}
+
+		@Override
+		public MateFeignSentinel.Builder contract(Contract contract) {
+			this.contract = contract;
+			return this;
+		}
+
+		@Override
+		public Feign build() {
+			super.invocationHandlerFactory(new InvocationHandlerFactory() {
+				@Override
+				public InvocationHandler create(Target target, Map<Method, MethodHandler> dispatch) {
+					// 查找 FeignClient 上的 降级策略
+					FeignClient feignClient = AnnotationUtils.findAnnotation(target.type(), FeignClient.class);
+					Class fallback = feignClient.fallback();
+					Class fallbackFactory = feignClient.fallbackFactory();
+
+					String beanName = feignClient.contextId();
+					if (!StringUtils.hasText(beanName)) {
+						beanName = feignClient.name();
+					}
+
+					Object fallbackInstance;
+					FallbackFactory fallbackFactoryInstance;
+					// check fallback and fallbackFactory properties
+					if (void.class != fallback) {
+						fallbackInstance = getFromContext(beanName, "fallback", fallback, target.type());
+						return new MateSentinelInvocationHandler(target, dispatch,
+								new FallbackFactory.Default(fallbackInstance));
+					}
+					if (void.class != fallbackFactory) {
+						fallbackFactoryInstance = (FallbackFactory) getFromContext(beanName, "fallbackFactory",
+								fallbackFactory, FallbackFactory.class);
+						return new MateSentinelInvocationHandler(target, dispatch, fallbackFactoryInstance);
+					}
+					return new MateSentinelInvocationHandler(target, dispatch);
+				}
+
+				private Object getFromContext(String name, String type, Class fallbackType, Class targetType) {
+					Object fallbackInstance = feignContext.getInstance(name, fallbackType);
+					if (fallbackInstance == null) {
+						throw new IllegalStateException(String.format(
+								"No %s instance of type %s found for feign client %s", type, fallbackType, name));
+					}
+
+					if (!targetType.isAssignableFrom(fallbackType)) {
+						throw new IllegalStateException(String.format(
+								"Incompatible %s instance. Fallback/fallbackFactory of type %s is not assignable to %s for feign client %s",
+								type, fallbackType, targetType, name));
+					}
+					return fallbackInstance;
+				}
+			});
+
+			super.contract(new SentinelContractHolder(contract));
+			return super.build();
+		}
+
+		@Override
+		public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+			this.applicationContext = applicationContext;
+			feignContext = this.applicationContext.getBean(FeignContext.class);
+		}
+
+	}
+
+}
