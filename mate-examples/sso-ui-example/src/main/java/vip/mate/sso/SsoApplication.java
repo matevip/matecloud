ec23da23ae9937e99f15fdbe329bba1f65792b9e
matevip <7333791@qq.com>
2021-05-20 22:54:54 +0800

    feature: 创建演示模块，首先增加单点登录的演示

---
+++ mate-examples/sso-ui-example/src/main/java/vip/mate/sso/SsoApplication.java
@@ -0,0 +1,16 @@
+package vip.mate.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 单点登录启动类
+ *
+ * @author pangu
+ */
+@SpringBootApplication
+public class SsoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SsoApplication.class, args);
+	}
+}
