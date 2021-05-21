b00900008e10a50736d79b5a8dcb68a73adf9199
matevip <7333791@qq.com>
2021-05-21 09:08:49 +0800

    feature: 完善单点登录模块样例

--- mate-examples/sso-ui-example/src/main/java/vip/mate/sso/SsoApplication.java
+++ mate-examples/sso-ui-example/src/main/java/vip/mate/sso/SsoApplication.java
@@ -2,12 +2,14 @@ package vip.mate.sso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import vip.mate.core.feign.annotation.EnableMateFeign;
 
 /**
  * 单点登录启动类
  *
  * @author pangu
  */
+@EnableMateFeign
 @SpringBootApplication
 public class SsoApplication {
 	public static void main(String[] args) {
