c67408123e4bfdea9db6da32bf1105e2e1f5e159
matevip <7333791@qq.com>
2021-05-26 07:01:32 +0800

    feature: 升级xxl-job至2.3.0版本

---
+++ mate-support/mate-job-admin/src/test/java/job/admin/util/I18nUtilTest.java
@@ -0,0 +1,22 @@
+package job.admin.util;
+
+import com.xxl.job.admin.core.util.I18nUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * email util test
+ *
+ * @author xuxueli 2017-12-22 17:16:23
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class I18nUtilTest {
+
+    @Test
+    public void test(){
+        System.out.println(I18nUtil.getString("admin_name"));
+        System.out.println(I18nUtil.getMultString("admin_name", "admin_name_full"));
+        System.out.println(I18nUtil.getMultString());
+    }
+
+}
