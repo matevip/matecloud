5169c1de3aed8c9e11e461a9de9d2e8a1a7bfd2f
张瑜 <yu.zhang15@geely.com>
2021-07-13 13:46:04 +0800

    [update] 线上环境不打印网关日志

--- mate-gateway/src/main/java/vip/mate/gateway/filter/RequestLogFilter.java
+++ mate-gateway/src/main/java/vip/mate/gateway/filter/RequestLogFilter.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.reactive.ServerHttpResponse;
@@ -26,6 +27,7 @@ import java.util.List;
 @Slf4j
 @Component
 @AllArgsConstructor
+@Profile({"!prod"})
 public class RequestLogFilter implements GlobalFilter, Ordered {
 
 	private static final String START_TIME = "startTime";
