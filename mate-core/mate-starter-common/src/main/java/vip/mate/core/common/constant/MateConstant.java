fa143beff30341338e5c12da5019345ad1619b7e
matevip <7333791@qq.com>
2021-08-15 18:32:11 +0800

    build: 发布4.0.8正式版

--- mate-core/mate-starter-common/src/main/java/vip/mate/core/common/constant/MateConstant.java
+++ mate-core/mate-starter-common/src/main/java/vip/mate/core/common/constant/MateConstant.java
@@ -13,7 +13,7 @@ public class MateConstant {
 	/**
 	 * 应用版本号
 	 */
-	public static final String MATE_APP_VERSION = "4.0.8-SNAPSHOT";
+	public static final String MATE_APP_VERSION = "4.0.8";
 
 	/**
 	 * Spring 应用名 prop key
