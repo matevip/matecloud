1d1228c7d75de5ff48e8a41758fee5b22a9ca0ed
matevip <7333791@qq.com>
2021-08-18 10:33:47 +0800

    refactor: 菜单管理增加检查id下是否包含子菜单

--- mate-platform/mate-system/src/main/java/vip/mate/system/service/ISysMenuService.java
+++ mate-platform/mate-system/src/main/java/vip/mate/system/service/ISysMenuService.java
@@ -18,44 +18,60 @@ import java.util.List;
  */
 public interface ISysMenuService extends IService<SysMenu> {
 
-	/**
-	 * 菜单路由
-	 *
-	 * @param roleId 角色ID
-	 * @return List
-	 */
-	List<SysMenuVO> routes(String roleId);
+    /**
+     * 菜单路由
+     *
+     * @param roleId 角色ID
+     * @return List
+     */
+    List<SysMenuVO> routes(String roleId);
 
-	/**
-	 * 查询列表
-	 *
-	 * @param search 检索对象
-	 * @return List
-	 */
-	List<SysMenu> searchList(Search search);
+    /**
+     * 查询列表
+     *
+     * @param search 检索对象
+     * @return List
+     */
+    List<SysMenu> searchList(Search search);
 
-	/**
-	 * 保存全部
-	 *
-	 * @param sysMenu 菜单对象
-	 * @return boolean
-	 */
-	boolean saveAll(SysMenu sysMenu);
+    /**
+     * 保存全部
+     *
+     * @param sysMenu 菜单对象
+     * @return boolean
+     */
+    boolean saveAll(SysMenu sysMenu);
 
-	/**
-	 * 状态设置
-	 *
-	 * @param ids    id列表
-	 * @param status 状态
-	 * @return boolean
-	 */
-	boolean status(String ids, String status);
+    /**
+     * 状态设置
+     *
+     * @param ids    id列表
+     * @param status 状态
+     * @return boolean
+     */
+    boolean status(String ids, String status);
 
-	/**
-	 * 菜单导出
-	 *
-	 * @return 菜单导出
-	 */
-	List<SysMenuPOI> export();
+    /**
+     * 菜单导出
+     *
+     * @return 菜单导出
+     */
+    List<SysMenuPOI> export();
+
+    /**
+     * 检查是否存在菜单子节点
+     *
+     * @param id 菜单ID
+     * @return 是否
+     */
+    boolean checkChild(Long id);
+
+    /**
+     * 根据菜单ID查询父类ID
+     *
+     * @param menuId 菜单ID
+     * @return parentId
+     */
+    int getMenuChild(Long menuId);
 
 }
