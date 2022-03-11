1f20d6b98e31986079e9446e4e2a0621ec4e2539
matevip <7333791@qq.com>
2022-03-11 19:44:19 +0800

    fix: 修复消费示例的异常和maven的版本

--- mate-mq/mate-message-producer/src/main/java/vip/mate/message/listener/OrderTransactionListener.java
+++ mate-mq/mate-message-producer/src/main/java/vip/mate/message/listener/OrderTransactionListener.java
@@ -23,53 +23,53 @@ import java.util.List;
  * @link https://blog.csdn.net/guzhangyu12345/article/details/107989633
  */
 @Slf4j
-@RocketMQTransactionListener(txProducerGroup = MessageConstant.ORDER_BINDER_GROUP)
+@RocketMQTransactionListener(rocketMQTemplateBeanName = MessageConstant.ORDER_BINDER_GROUP)
 public class OrderTransactionListener implements RocketMQLocalTransactionListener {
 
-	/**
-	 * rocketmq 消息发送成功之后，提交本地事务
-	 *
-	 * @param message 消息
-	 * @param o       　args
-	 * @return RocketMQLocalTransactionState
-	 */
-	@Override
-	public RocketMQLocalTransactionState executeLocalTransaction(Message message, Object o) {
+    /**
+     * rocketmq 消息发送成功之后，提交本地事务
+     *
+     * @param message 消息
+     * @param o       　args
+     * @return RocketMQLocalTransactionState
+     */
+    @Override
+    public RocketMQLocalTransactionState executeLocalTransaction(Message message, Object o) {
 
-		MessageHeaders headers = message.getHeaders();
-		String transactionId = String.valueOf(headers.get(RocketMQHeaders.TRANSACTION_ID));
-		Long shareId = Long.valueOf(String.valueOf(headers.get("share_id")));
-		Order order = null;
-		try {
-			order = JSONObject.parseObject(new String((byte[]) message.getPayload()), Order.class);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        MessageHeaders headers = message.getHeaders();
+        String transactionId = String.valueOf(headers.get(RocketMQHeaders.TRANSACTION_ID));
+        Long shareId = Long.valueOf(String.valueOf(headers.get("share_id")));
+        Order order = null;
+        try {
+            order = JSONObject.parseObject(new String((byte[]) message.getPayload()), Order.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
 
-		Long args = (Long) o;
-		log.info(String.format("half message\npayload:%s, arg:%s, transactionId:%s", order, args, message.getHeaders().get(RocketMQHeaders.TRANSACTION_ID)));
-		return RocketMQLocalTransactionState.COMMIT;
+        Long args = (Long) o;
+        log.info(String.format("half message\npayload:%s, arg:%s, transactionId:%s", order, args, message.getHeaders().get(RocketMQHeaders.TRANSACTION_ID)));
+        return RocketMQLocalTransactionState.COMMIT;
 
-	}
+    }
 
-	/**
-	 * rocketmq 回查,判断提交还是回滚
-	 *
-	 * @param message Message
-	 * @return RocketMQLocalTransactionState
-	 */
-	@Override
-	public RocketMQLocalTransactionState checkLocalTransaction(Message message) {
-		Order order = JSON.parseObject(new String((byte[]) message.getPayload()), Order.class);
-		// 业务查询本地事务是否执行成功
-		List<Order> orders = new ArrayList<>();
-		orders.add(order);
-		// 根据message去查询本地事务是否执行成功，如果成功，则commit
-		if (orders.size() > 0) {
-			return RocketMQLocalTransactionState.COMMIT;
-		} else {
-			return RocketMQLocalTransactionState.ROLLBACK;
-		}
-	}
+    /**
+     * rocketmq 回查,判断提交还是回滚
+     *
+     * @param message Message
+     * @return RocketMQLocalTransactionState
+     */
+    @Override
+    public RocketMQLocalTransactionState checkLocalTransaction(Message message) {
+        Order order = JSON.parseObject(new String((byte[]) message.getPayload()), Order.class);
+        // 业务查询本地事务是否执行成功
+        List<Order> orders = new ArrayList<>();
+        orders.add(order);
+        // 根据message去查询本地事务是否执行成功，如果成功，则commit
+        if (orders.size() > 0) {
+            return RocketMQLocalTransactionState.COMMIT;
+        } else {
+            return RocketMQLocalTransactionState.ROLLBACK;
+        }
+    }
 }
