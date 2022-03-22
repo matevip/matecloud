80e834de53de537b34453a314515c308bad6cbef
matevip <7333791@qq.com>
2022-03-22 18:05:43 +0800

    fix: 修复字段重复引入的问题

--- mate-support/mate-code/src/main/java/vip/mate/code/entity/Column.java
+++ mate-support/mate-code/src/main/java/vip/mate/code/entity/Column.java
@@ -155,26 +155,6 @@ public class Column extends BaseEntity {
 	*/
 	@ApiModelProperty(value = "显示顺序")
 	private Integer sort;
-	/**
-	* 创建者
-	*/
-	@ApiModelProperty(value = "创建者")
-	private Long createBy;
-	/**
-	* 创建时间
-	*/
-	@ApiModelProperty(value = "创建时间")
-	private LocalDateTime createTime;
-	/**
-	* 更新者
-	*/
-	@ApiModelProperty(value = "更新者")
-	private Long updateBy;
-	/**
-	* 更新时间
-	*/
-	@ApiModelProperty(value = "更新时间")
-	private LocalDateTime updateTime;
 
 
 }
--- mate-support/mate-code/src/main/java/vip/mate/code/entity/Table.java
+++ mate-support/mate-code/src/main/java/vip/mate/code/entity/Table.java
@@ -120,26 +120,5 @@ public class Table extends BaseEntity {
 	*/
 	@ApiModelProperty(value = "备注")
 	private String remark;
-	/**
-	* 创建者
-	*/
-	@ApiModelProperty(value = "创建者")
-	private Long createBy;
-	/**
-	* 创建时间
-	*/
-	@ApiModelProperty(value = "创建时间")
-	private LocalDateTime createTime;
-	/**
-	* 更新者
-	*/
-	@ApiModelProperty(value = "更新者")
-	private Long updateBy;
-	/**
-	* 更新时间
-	*/
-	@ApiModelProperty(value = "更新时间")
-	private LocalDateTime updateTime;
-
 
 }
