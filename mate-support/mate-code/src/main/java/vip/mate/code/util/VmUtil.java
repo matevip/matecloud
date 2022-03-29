1c7378a8089f70d42d571048106cb88146aab8e3
david <david@daotiandi.cn>
2022-03-30 07:47:10 +0800

    feat: 代码生成模板

---
+++ mate-support/mate-code/src/main/java/vip/mate/code/util/VmUtil.java
@@ -0,0 +1,26 @@
+package vip.mate.code.util;
+
+/**
+ * 模板工具类
+ *
+ * @author david
+ * @since 2022-3-29
+ */
+public class VmUtil {
+
+    /**
+     * 项目空间路径
+     */
+    private static final String PROJECT_PATH = "main/java" ;
+
+    /**
+     * 隐藏字段数组
+     */
+    private static final String HIDE = "hide" ;
+
+    /**
+     * 覆写字段数组
+     */
+    private static final String COVER = "cover" ;
+
+}
